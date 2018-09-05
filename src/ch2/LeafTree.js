39cc4bc5e2327ad82718317f8f1b921d3ed2cbcc
Letladi Sebesho <sletladi@gmail.com>
2018-09-05 04:36:49 +0200

    implemented basic operations for the LeafTree

--- src/ch2/LeafTree.js
+++ src/ch2/LeafTree.js
@@ -2,6 +2,33 @@ const assert = require('assert')
 const Node = require('./Node')
 const { isNull } = require('../util/')
 
+const assertLeftRotationConditions = (node) => {
+  assert(!node.isLeaf(), 'can only perform left rotation on interior error')
+  assert(!node.right.isLeaf(), 'can only perform left rotation if node.right is interior node')
+}
+
+const assertRightRotationConditions = node => {
+  assert(!node.isLeaf(), 'can only perform left rotation on interior error')
+  assert(!node.left.isLeaf(), 'can only perform left rotation if node.right is interior node')
+}
+
+const swapKeys = (node1, node2) => {
+  const tempKey = node1.key
+  node1.key = node2.key
+  node2.key = tempKey
+}
+
+const isLeaf = () => {
+  return isNull(this.right)
+}
+
+const copyNode = (target, source) => {
+  target.key = source.key
+  target.left = source.left
+  target.right = source.right
+  return target
+}
+
 class LeafTree {
   constructor() {
     this.root = new Node()
@@ -10,17 +37,107 @@ class LeafTree {
   isEmpty() {
     return isNull(this.root.left)
   }
-
+  /*
+    current interval = node.left U node.right.left U node.right.right
+    This method groups the first two intervals into A (node.left & node.right.left),
+    and then groups the merged intervals with the last interval (node.right.right)
+  */
   rotateLeft(node) {
-    assert(node.isLeaf() === false, 'can only perform left rotation on interior error')
-    assert(node.right.isLeaf() === false, 'can only perform left rotation if node.right is interior node')
-    const temp = node.left
-    const tempKey = node.key
-    node.left = node.right
-    node.key = node.right.key
-    node.right = node.left.right
-    node.left.left = temp
-    node.left.key = tempKey
+    assertLeftRotationConditions(node)
 
+    swapKeys(node, node.right)
+    const temp = node.left
+
+    node.left = node.right
+    node.right = node.right.right
+
+    node.left.right = node.left.left
+    node.left.left = temp
+
+    return node
+  }
+
+  rotateRight(node) {
+    assertRightRotationConditions(node)
+
+    swapKeys(node, node.left)
+    const temp = node.right
+
+    node.right = node.left
+    node.left = node.left.left
+
+    node.right.left = node.right.right
+    node.right.right = temp
+
+    return node
+  }
+
+  find(key) {
+    if (this.isEmpty()) return null
+    let current = this.root
+    while (!isLeaf(current)) {
+      current = (key < current.key) ? current.left : current.right
+    }
+    return (current.key === key) ? current.left : null
+  }
+
+  insert(key, val) {
+    if (this.isEmpty()) this.root = new Node(key, val)
+    else {
+      let current = this.root
+      while (!isLeaf(current)) {
+        current = (key < current.key) ? current.left : current.right
+      }
+      /* duplicates keys not allowed */
+      if (current.key === key)
+        return false
+      else {
+        let oldLeaf = new Node(current.key, current.left)
+        const newLeaf = new Node(key, val)
+        if (current.key < key) {
+          current.left = oldLeaf
+          current.right = newLeaf
+          current.key = key
+        } else {
+          current.left = newLeaf
+          current.right = oldLeaf
+        }
+      }
+    }
+    return true
+  }
+
+  delete(key) {
+    if (this.isEmpty())
+      return null
+    else if (isLeaf(this.root)) {
+      if (this.root.key === key) {
+        const deleteVal = this.root.left
+        this.root.left = null
+        return deleteVal
+      } else
+        return null
+    } else {
+      let current = this.root
+      let currentParent = null
+      let currentSibling = null
+
+      while (!isLeaf(current)) {
+        currentParent = current
+        if (key < current.key) {
+          current = currentParent.left
+          currentSibling = currentParent.right
+        } else {
+          current = currentParent.right
+          currentSibling = currentParent.left
+        }
+      }
+      if (current.key !== key)
+        return null
+      else {
+        copyNode(currentParent, currentSibling)
+        return current.left
+      }
+    }
   }
 }
--- src/ch2/LeafTree.spec.js
+++ src/ch2/LeafTree.spec.js
@@ -3,8 +3,39 @@ const LeafTree = require('./LeafTree')
 const Node = require('./Node')
 
 describe('LeafTree', () => {
-  describe('#rotateLeft', () => {
+  describe('rotateLeft()', () => {
     it('only performs rotation on interior node')
     it('only performs rotation if node.right is an interior node')
+    it('maintains valid key order relation')
+  })
+  describe('rotateRight()', () => {
+    it('only performs rotation on interior node')
+    it('only performs rotation if node.left is an interior node')
+    it('maintains valid key order relation')
+  })
+  describe('#height', () => {
+    it('should be >= Math.ceil(log n)')
+    it('should be <= n - 1')
+    it('returns the maximum height of the tree')
+  })
+  describe('#depth', () => {
+    it('should be >= log n')
+    it('should be <= (n - 1)(n + 2) / 2n (approx. 0.5n)')
+  })
+  describe('#find', () => {
+    it('returns null if the tree is empty')
+    it('returns null if the a value associated with the query key is not in the tree')
+    it('returns the value associated with the query key if it exists in the tree')
+  })
+  describe('#insert', () => {
+    it('returns false if we attempt to insert a key that already exists in the tree')
+    it('returns true if insertion succeeded')
+    it('should be able to insert in empty tree')
+    it('should be able to insert in tree with existing node')
+    it('increases the leaveCount of the tree')
+  })
+  describe('#delete', () => {
+    it('returns null is deletion failed (like when tree is empty)')
+    it('returns the value associated with the deleted key')
   })
 })
